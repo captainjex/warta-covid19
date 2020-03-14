33760a524ca8a0e4a0158287b4cf87442f99d53a
wkwksama <mailpakgasing@gmail.com>
2020-03-14 22:48:20 +0700

    gitlab ci and ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,18 @@
+// pm2 config
+module.exports = {
+  apps: [{
+    name: 'warta-covid19',
+    script: 'node',
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '200M',
+    env: {
+      NODE_ENV: 'development',
+    },
+    env_production: {
+      NODE_ENV: 'production',
+    },
+  }],
+};
