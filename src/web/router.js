3c36af1e2d7235c519b1d493b6fb79fbfa2ed6b1
wkwksama <mailpakgasing@gmail.com>
2020-03-14 19:22:09 +0700

    fix web: handle catch error

--- src/web/router.js
+++ src/web/router.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import * as kemkes from '../lib/services/kemkes';
 
+const handleRes = (promise) => (req, res) => {
+  promise(req, res).catch((error) => {
+    res.status(500).send(error.message);
+  });
+};
+
 const router = express.Router();
 
-router.get('/kemkes/posts', async (req, res) => {
+router.get('/kemkes/posts', handleRes(async (req, res) => {
   const posts = await kemkes.getCoronaPosts();
-  res.send(posts);
-});
+  res.render('kemkes/index', { posts });
+}));
 
-router.get('/kemkes/posts/:id', async (req, res) => {
+router.get('/kemkes/posts/:id', handleRes(async (req, res) => {
   const post = await kemkes.getPost(req.params.id);
-  res.send(post);
-});
+  res.render('kemkes/detail', { post });
+}));
 
 export default router;
