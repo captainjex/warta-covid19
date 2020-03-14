57a65cb7d8fbf85e18202f7dbe9c958e68396997
wkwksama <mailpakgasing@gmail.com>
2020-03-14 17:32:08 +0700

    page posts and detail: styling with tailwind

--- src/web/index.js
+++ src/web/index.js
@@ -1,15 +1,22 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import moment from 'moment';
+import 'moment/locale/id';
 import router from './router';
 
-const server = express();
+moment.locale('id');
 
+const server = express();
 server.set('views', path.join(__dirname, '../../resources/views/pages'));
 server.set('view engine', 'pug');
 server.use('/assets', express.static(path.join(__dirname, '../../resources/assets')));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+server.use((req, res, next) => {
+  res.locals.moment = moment;
+  next();
+});
 
 server.get('/', (req, res) => {
   res.send('ok');
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  theme: {
+    colors: {
+      white: '#ffffff',
+      black: '#000000',
+      shark: '#22272C',
+      gunmetal: '#2E343B',
+      zircon: '#f1f2f3',
+    },
+    extend: {
+      fontFamily: {
+        body: ['IBM Plex Sans', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
